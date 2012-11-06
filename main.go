39c784e6d941398ff85d3adecf3bf2f5b1e88561
Jim Arnold <jim@thoughtworks.com>
2012-11-06 14:22:34 -0500

    extracted Palette to its own file

--- main.go
+++ main.go
@@ -55,37 +55,6 @@ func main() {
   }
 }
 
-type RGB struct {
-  r uint8
-  g uint8
-  b uint8
-}
-
-type Palette struct {
-  colors []RGB
-}
-
-func NewPalette() *Palette {
-  palette := &Palette{}
-  palette.colors = make([]RGB, 256)
-  var r uint8 = 0
-  var g uint8 = 0
-  var b uint8 = 31
-  for i:=0;i<=255;i++ {
-    if r < 255 {
-      r++
-    }
-    if g < 255 {
-      g++
-    }
-    if b < 255 {
-      b++
-    }
-    palette.colors[i] = RGB{r,g,b}
-  }
-  return palette
-}
-
 func reseed(plot *Plot, attractor *Attractor) {
   attractor.seed()
   plot.Clear()
